75457c0d6a1932ba0baf11ce30a9a4c0884d71f5
melvinorellana123 <melvin016orellana@gmail.com>
2023-04-22 21:07:10 -0600

    first commit

---
+++ Administrador de Tareas/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Administrador_de_Tareas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
